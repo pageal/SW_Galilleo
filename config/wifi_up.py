97da807d5c79db15e2cf36d53ef2ac41fc926fbf
Intel Makers <intelmakers@gmail.com>
2015-09-05 19:07:43 +0300

    wifi_up.py fix

--- config/wifi_up.py
+++ config/wifi_up.py
@@ -1,8 +1,10 @@
 import os
 import time
 
-time.sleep(5)
+time.sleep(10)
 os.system("rfkill unblock all")
-time.sleep(5)
+time.sleep(10)
+os.system("/usr/sbin/wpa_supplicant -u -iwlp0s20f3u1 -c/etc/wpa_supplicant.conf &")
+time.sleep(10)
 os.system("ifup wlp0s20f3u1")
 
